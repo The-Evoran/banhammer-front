b3f1dc684b747dd55434b5d931669fc594c88380
the-evoran <nicoloayquentin@gmail.com>
2022-12-10 00:44:43 +0100

    Changes to be committed: 	modified:   config/database.js 	new file:   serverless.yml

--- config/database.js
+++ config/database.js
@@ -3,7 +3,10 @@ const Sequelize = require("sequelize");
 // // Sequelize connexion
 const sequelize = new Sequelize("banhammer", "test", "j'c74PPFPXjG*cTS", {
   host: "34.140.85.168",
-  dialect: "mysql"
+  dialect: "mysql",
+  dialectOptions: {
+    socketPath: '/cloudsql/34.140.85.168'
+},
 });
 
 module.exports = sequelize;
