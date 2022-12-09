085867b8096495ea6def4559577d5a80c8de4ba9
the-evoran <nicoloayquentin@gmail.com>
2022-12-10 00:59:27 +0100

    change app

--- config/database.js
+++ config/database.js
@@ -1,12 +1,13 @@
 const Sequelize = require("sequelize");
 
 // // Sequelize connexion
-const sequelize = new Sequelize("banhammer", "test", "j'c74PPFPXjG*cTS", {
-  host: "34.140.85.168",
-  dialect: "mysql",
+
+const sequelize = new Sequelize('banhammer', 'test', 'j\'c74PPFPXjG*cTS', {
+  dialect: 'mysql',
+  host: '/cloudsql/banhammher:europe-west1:banhammer',
+  timestamps: false,
   dialectOptions: {
     socketPath: '/cloudsql/banhammher:europe-west1:banhammer'
 },
 });
-
 module.exports = sequelize;
